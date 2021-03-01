1796f2a625985c9bb3e49e23adc6176732d905ef
Keaton13 <bajabuggy313@gmail.com>
2021-02-28 16:24:15 -0800

    Added row height to sign-up and added top margin to rows

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Header from './header';
-import SignIn from './sign-in';
+import SignUp from './sign-up';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -22,6 +22,6 @@ export default class App extends React.Component {
   render() {
     return this.state.isLoading
       ? <h1>Testing connections...</h1>
-      : <SignIn />;
+      : <SignUp />;
   }
 }
--- client/components/sign-up.jsx
+++ client/components/sign-up.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import Header from './header';
 
-class SignIn extends React.Component {
+class SignUp extends React.Component {
   render() {
     return (
       <div>
         <Header />
         <div className="container">
-          <div className="row font-2 background-color-1">
+          <div className="row font-2 background-color-1 sign-up-row-height">
             <div className="col text-center my-auto">
               <h2>Sign-Up</h2>
             </div>
           </div>
-          <div className="row w-75 mx-auto">
-            <div className="row w-100">
-              <h3>Username</h3>
-              <div className="input-group mb-3">
+          <div className="row w-75 mx-auto mt-3">
+            <div className="row w-100 mt-3">
+              <h3 className="font-2">Username</h3>
+              <div className="input-group input-group-lg">
                 <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
               </div>
             </div>
-            <div className="row w-100">
-              <h3>Password</h3>
-              <div className="input-group mb-3">
+            <div className="row w-100 mt-3">
+              <h3 className="font-2">Password</h3>
+              <div className="input-group input-group-lg">
                 <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
               </div>
             </div>
-            <div className="row w-100">
-              <h3>Re-Enter Password</h3>
-              <div className="input-group mb-3">
+            <div className="row w-100 mt-3">
+              <h3 className="font-2">Re-Enter Password</h3>
+              <div className="input-group input-group-lg">
                 <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
               </div>
             </div>
-            <div className="row w-100">
+            <div className="row w-100 mt-5">
               <button type="button" className="btn btn-primary btn-lg btn-block w-75 mx-auto">Submit</button>
             </div>
           </div>
@@ -40,4 +40,4 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+export default SignUp;
